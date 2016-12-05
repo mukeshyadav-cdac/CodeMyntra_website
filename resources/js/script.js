9e0f7e9a81774e4029df8b6421221b530a3d34d2
Mukesh <ashu.3333d@gmail.com>
2016-12-05 08:25:08 +0530

    updated site code.

---
+++ resources/js/script.js
@@ -0,0 +1,27 @@
+$(function (argument) {
+
+  var waypoint = new Waypoint({
+    element: $('.js--section-how-we-do'),
+    handler: function(direction) {
+      if (direction == 'down' ) {
+        $('nav').addClass('sticky');
+      } else {
+        $('nav').removeClass('sticky');
+      }
+    },
+    offset: 60
+  });
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+})
